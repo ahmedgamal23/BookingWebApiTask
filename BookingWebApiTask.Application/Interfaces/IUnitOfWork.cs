c57ab5b148f74b50fbe18a5270db4d717bb2a782
ahmed_gamal <am5679456@gmail.com>
2025-03-24 14:12:51 +0200

    apply UnitOfWork design pattern. create IUnitOfWok then implement UnitOfWork class.

---
+++ BookingWebApiTask.Application/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,20 @@
+﻿using BookingWebApiTask.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingWebApiTask.Application.Interfaces
+{
+    public interface IUnitOfWork:IDisposable
+    {
+        public IBaseRepository<ApplicationUser, string> ApplicationUser { get; }
+
+        public IBaseRepository<Reservation, int> Reservation { get; }
+
+        public IBaseRepository<Trip, int> Trip { get; }
+
+        public Task<int> SaveChangesAsync();
+    }
+}
---
+++ BookingWebApiTask.Infrastructure/Repository/UnitOfWork.cs
@@ -0,0 +1,39 @@
+﻿using BookingWebApiTask.Application.Interfaces;
+using BookingWebApiTask.Domain.Data;
+using BookingWebApiTask.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingWebApiTask.Infrastructure.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext _context;
+        public IBaseRepository<ApplicationUser, string> ApplicationUser { get; private set; }
+
+        public IBaseRepository<Reservation, int> Reservation { get; private set; }
+
+        public IBaseRepository<Trip, int> Trip { get; private set; }
+
+        public UnitOfWork(ApplicationDbContext context)
+        {
+            _context = context;
+            ApplicationUser = new BaseRepository<ApplicationUser, string>(_context);
+            Reservation = new BaseRepository<Reservation, int>(_context);
+            Trip = new BaseRepository<Trip, int>(_context);
+        }
+
+        public void Dispose()
+        {
+            _context.Dispose();
+        }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await _context.SaveChangesAsync();
+        }
+    }
+}
